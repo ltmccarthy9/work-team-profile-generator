8e97fa4bebdaf3deffdce34ec47e07a8686a35f8
Liam McCarthy <ltmccarthy9@gmail.com>
2022-07-20 23:13:03 -0500

    javascript mostly done, have to add <a> to email and github

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const fs = require("fs");
-const inquirer = require('inquirer');
-const Employee = require('./lib/employee');
-const Manager = require('./lib/manager');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
-const { generate } = require("rxjs");
-const { create } = require("domain");
 
 const Generate = require('./lib/generate');
-var managers = 0;
+
 
 
 const go = new Generate;
